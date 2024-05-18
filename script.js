d8e026743ad6170efaa3bdb313a6ab34bf1d76f4
MaksyDarko <darko.punktooy@gmail.com>
2024-05-18 17:17:22 +0200

    -added wather advice

---
+++ script.js
@@ -0,0 +1,11 @@
+let weatherAdvices = "snowy";
+
+if (weatherAdvices === "sunny") {
+  console.log("Wear sunglasses");
+} else if (weatherAdvices === "rainy") {
+  console.log("Bring an umbrella");
+} else if (weatherAdvices === "snowy") {
+  console.log("Wear a warm coat!");
+} else {
+  console.log("Check the weather forecast for more details");
+}
